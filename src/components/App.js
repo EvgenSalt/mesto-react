80f199a87fb4fce68b55d4b30e8d9f6e31f5e9a2
Evgeniy Saltykov <saltykov.evgen@mail.ru>
2023-02-09 13:03:29 +0300

    init CurrentUserContext.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Main from './Main';
 import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 import { api } from '../utils/api';
+import CurrentUserContext from '../contexts/CurrentUserContext';
 
 function App() {
 
@@ -71,75 +72,77 @@ function App() {
   }
 
   return (
-    <div className="page">
-      <Header />
-      <Main
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onEditAvatar={handleEditAvatarClick}
-        userName={userName}
-        userDescription={userDescription}
-        userAvatar={userAvatar}
-        cards={cards}
-        onCardClick={handleCardClick}
-      />
-      <Footer />
-      <ImagePopup
-        card={selectedCard}
-        onClose={closeAllPopups}
-      />
-      <PopupWithForm
-        isOpen={isEditAvatarPopupOpen}
-        name={'avatar-edit'}
-        title={'Обновить аватар'}
-        children={
-          <>
-            <input type="url" name="link_img" placeholder="Ссылка на картинку" className="form__input form__input_text_src-img" defaultValue="" required id="link-avatar" />
-            <span className="form__msg" id="link-avatar-error">error</span>
-          </>}
-        btnText={'Сохранить'}
-        onClose={closeAllPopups}
-      />
-      <PopupWithForm
-        isOpen={isEditProfilePopupOpen}
-        name={'edit'}
-        title={'Редактировать профиль'}
-        children={
-          <>
-            <input type="text" name="username" placeholder="name" className="form__input form__input_text_name" defaultValue=""
-              required minLength="2" maxLength="40" id="username" />
-            <span className="form__msg" id="username-error">error</span>
-            <input type="text" name="userwork" placeholder="work" className="form__input form__input_text_work" defaultValue=""
-              required minLength="2" maxLength="200" id="userwork" />
-            <span className="form__msg" id="userwork-error">error</span>
-          </>}
-        btnText={'Сохранить'}
-        onClose={closeAllPopups}
-      />
-      <PopupWithForm
-        isOpen={isAddPlacePopupOpen}
-        name={'add'}
-        title={'Новое место'}
-        children={
-          <>
-            <input type="text" name="name_img" placeholder="Название" className="form__input form__input_text_name-img" defaultValue=""
-              required minLength="2" maxLength="30" id="name-img" />
-            <span className="form__msg" id="name-img-error">error</span>
-            <input type="url" name="link_img" placeholder="Ссылка на картинку" className="form__input form__input_text_src-img"
-              defaultValue="" required id="link-img" />
-            <span className="form__msg" id="link-img-error">error</span>
-          </>}
-        btnText={'Создать'}
-        onClose={closeAllPopups}
-      />
-      <PopupWithForm
-        isOpen={false}//TO DO
-        name={'delet-img'}
-        title={'Вы уверены?'}
-        btnText={'Да'}
-        onClose={closeAllPopups}
-      />
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Header />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          userName={userName}
+          userDescription={userDescription}
+          userAvatar={userAvatar}
+          cards={cards}
+          onCardClick={handleCardClick}
+        />
+        <Footer />
+        <ImagePopup
+          card={selectedCard}
+          onClose={closeAllPopups}
+        />
+        <PopupWithForm
+          isOpen={isEditAvatarPopupOpen}
+          name={'avatar-edit'}
+          title={'Обновить аватар'}
+          children={
+            <>
+              <input type="url" name="link_img" placeholder="Ссылка на картинку" className="form__input form__input_text_src-img" defaultValue="" required id="link-avatar" />
+              <span className="form__msg" id="link-avatar-error">error</span>
+            </>}
+          btnText={'Сохранить'}
+          onClose={closeAllPopups}
+        />
+        <PopupWithForm
+          isOpen={isEditProfilePopupOpen}
+          name={'edit'}
+          title={'Редактировать профиль'}
+          children={
+            <>
+              <input type="text" name="username" placeholder="name" className="form__input form__input_text_name" defaultValue=""
+                required minLength="2" maxLength="40" id="username" />
+              <span className="form__msg" id="username-error">error</span>
+              <input type="text" name="userwork" placeholder="work" className="form__input form__input_text_work" defaultValue=""
+                required minLength="2" maxLength="200" id="userwork" />
+              <span className="form__msg" id="userwork-error">error</span>
+            </>}
+          btnText={'Сохранить'}
+          onClose={closeAllPopups}
+        />
+        <PopupWithForm
+          isOpen={isAddPlacePopupOpen}
+          name={'add'}
+          title={'Новое место'}
+          children={
+            <>
+              <input type="text" name="name_img" placeholder="Название" className="form__input form__input_text_name-img" defaultValue=""
+                required minLength="2" maxLength="30" id="name-img" />
+              <span className="form__msg" id="name-img-error">error</span>
+              <input type="url" name="link_img" placeholder="Ссылка на картинку" className="form__input form__input_text_src-img"
+                defaultValue="" required id="link-img" />
+              <span className="form__msg" id="link-img-error">error</span>
+            </>}
+          btnText={'Создать'}
+          onClose={closeAllPopups}
+        />
+        <PopupWithForm
+          isOpen={false}//TO DO
+          name={'delet-img'}
+          title={'Вы уверены?'}
+          btnText={'Да'}
+          onClose={closeAllPopups}
+        />
+      </div>
+    </CurrentUserContext.Provider >
   );
 }
 
